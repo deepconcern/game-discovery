9c3017e691ac6748b312422e3f7c4c7d2e52daa7
Wyatt Barnes <wyattlbarnes@gmail.com>
2020-05-24 17:35:31 -0700

    Adding CORS for app

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,14 +1,21 @@
 import { ApolloServer } from 'apollo-server';
+import { config } from 'dotenv';
 
 import { RAWGDataSource } from './data-sources';
 import { getSchema } from './get-schema';
 import { gameResolver, queryResolver } from './resolvers';
+
+config();
 
 const PORT = process.env['PORT'] || 4000;
 
 const typeDefs = getSchema();
 
 const server = new ApolloServer({
+    cors: {
+        origin: process.env.APP_HOSTNAME,
+        optionsSuccessStatus: 200
+    },
     dataSources: () => ({
         rawg: new RAWGDataSource(),
     }),
